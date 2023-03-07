15fcb268ff0203d1d6f52b3c4a565f676c76960b
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2023-03-07 17:22:49 +0000

    implemented click functionality

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
+const board = document.querySelector('.board');
+document.addEventListener("DOMContentLoaded", (event) => {
+  board.classList.add('x');
+});
+window.addEventListener("load", (event) => {
+
+});
+
 const cellElements = document.querySelectorAll('[data-cell]');
 cellElements.forEach(cell => {
   cell.addEventListener('click', handleClick, { once: true });
 });
 
 function handleClick(e) {
-  console.log(e.target);
+  const turn = checkTurn();
+  const cell = e.target;
+  cell.classList.add(turn);
+  switchTurn();
+}
+
+function checkTurn(){
+  if(board.classList.contains('x')){
+    return 'x';
+  }else{
+    return 'circle';
+  }
+}
+
+function switchTurn(){
+  board.classList.toggle('x');
+  board.classList.toggle('circle');
 }
