d914bfebf1563a70edc5e5c3e38fe06840cf3089
Ibrahim Hashem <ibrahimsgh6@hotmail.co.uk>
2023-03-10 18:55:16 +0000

    implemented win functionality and button reset

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const board = document.querySelector('.board');
+const winnerModal = document.querySelector('.winner-modal');
+const button = winnerModal.querySelector('button');
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 document.addEventListener("DOMContentLoaded", (event) => {
   board.classList.add('x');
 });
-window.addEventListener("load", (event) => {
 
+button.addEventListener('click', () => {
+  location.reload();
 });
 
 const cellElements = document.querySelectorAll('[data-cell]');
@@ -12,10 +25,23 @@ cellElements.forEach(cell => {
 });
 
 function handleClick(e) {
-  const turn = checkTurn();
   const cell = e.target;
+  const turn = checkTurn();
   cell.classList.add(turn);
+  checkWin(cell.parentElement,turn);
+
   switchTurn();
+}
+
+function checkWin(parent,turn){
+  const cells = parent.querySelectorAll('[data-cell]');
+  WINNING_COMBINATIONS.forEach(combination => {
+    const [a,b,c]=combination;
+    if(cells[a].classList.contains(turn) && cells[b].classList.contains(turn) && cells[c].classList.contains(turn)){
+      winnerModal.classList.add('show');
+      winnerModal.querySelector('.winner-message').innerHTML = `${turn} won`;
+    }
+  });
 }
 
 function checkTurn(){
